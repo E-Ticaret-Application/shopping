d13a6b647bd7a467fc9d4b4436bbd3658cf2a54a
Fatih Yavuz Gurel <f.gurel@demsay.com>
2023-07-30 16:32:31 +0300

    BaseEntity Id

--- eticaret/eticaret/src/main/java/com/shopping/BaseEntity.java
+++ eticaret/eticaret/src/main/java/com/shopping/BaseEntity.java
@@ -1,4 +1,12 @@
 package com.shopping;
 
+import jakarta.persistence.*;
+
 public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
+    @SequenceGenerator(name = "id_sequence", sequenceName = "id_sequence", allocationSize = 1)
+    @Column(name = "id", precision = 18)
+    protected Long id;
 }
